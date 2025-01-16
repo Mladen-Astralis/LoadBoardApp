148e250c7daecba55815aa4744ccefe30056a955
Mladen-Astralis <mladen@astralis-digital.com>
2025-01-16 14:59:59 +0100

    #

---
+++ LoadBoardApp.Common/Extensions/StringExtensions.cs
@@ -0,0 +1,8 @@
+﻿namespace LoadBoardApp.Common.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Truncate(this string value, int maxLength)
+            => value?.Length > maxLength ? value.Substring(0, maxLength) + "..." : value;
+    }
+}
